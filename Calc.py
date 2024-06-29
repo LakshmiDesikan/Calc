bd3ab8ee7c8ebeca3862708b4acb80fb40c249ec
LakshmiDesikan <151818232+LakshmiDesikan@users.noreply.github.com>
2024-06-29 23:07:41 +0530

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a Number: "))
+b=int(input("Enter a Number: "))
+add=a+b
+print(add, "is a Addtion Number.")
+sub=a-b
+print(sub, "is a SUbtration Number.")
+mul=a*b
+print(mul, "is a Multiplication Number.")
+div=a/b
+print(div, "is a Division Number.")
+square=int(a*a)
+cube=float(b*b*b)
+if (square>cube):
+    print(square, "is a Greater than value.")
+else:
+    print(cube, "is a Greater than value. ")
