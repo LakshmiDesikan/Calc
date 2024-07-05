e08b73fd28d62b87ba9eba9e668bebbec45ed027
LakshmiDesikan <151818232+LakshmiDesikan@users.noreply.github.com>
2024-07-05 12:13:12 +0530

    Add files via upload

---
+++ python-json.py
@@ -0,0 +1,28 @@
+A={
+    "employee":{
+        "name":"Lakshmi",
+        "age" :"23",
+        "salary":"20000",
+        "gender":"Female",
+        "marrital_status":False
+    }
+}
+name=A["employee"]["name"]
+age=A["employee"]["age"]
+salary=A["employee"]["salary"]
+gender=A["employee"]["gender"]
+marrital_status=A["employee"]["marrital_status"]
+
+if gender=="male":
+    he_or_she="He"
+else:
+    he_or_she="She"
+
+if marrital_status==True:
+    marrital_status="Married"
+else:
+    marrital_status="Unmarried"
+
+print(f"{name} is earning {salary} and {he_or_she} is {marrital_status}")
+                                                                                                                                                                                                     
+                                                                                                                                                              
